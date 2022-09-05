89a1993d7e4fd81626891c895619bde80e60eee7
TamTD <phiphongbee108@gmail.com>
2022-09-05 14:49:34 +0700

    LAB 04

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -18,7 +18,7 @@ class Menu extends Component {
     const menu = this.props.dishes.map((dish) => {
       return (
         <div className="col-12 col-md-5 m-1" key={dish.id}>
-          <Card onClick={() => this.onDishSelect(dish)}>
+          <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
             <CardImg width="100%" src={dish.image} value={dish.name} />
             <CardImgOverlay>
               <CardTitle>{dish.name}</CardTitle>
